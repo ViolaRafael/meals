f6f032511b5f927b083dca632c9182e52ef54a72
ViolaRafael <rafaelviola.eprosec@gmail.com>
2022-12-12 15:01:44 -0100

    Add a widget to show meals

--- lib/models/meals.dart
+++ lib/models/meals.dart
@@ -17,7 +17,7 @@ class Meal {
   final String imageUrl;
   final List<String> ingredients;
   final List<String> steps;
-  final int  duration;
+  final int duration;
   final bool isGlutenFree;
   final bool isLactoseFree;
   final bool isVegan;
@@ -39,5 +39,29 @@ class Meal {
     required this.isVegetarian,
     required this.complexity,
     required this.cost,
-});
+  });
+
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.simple:
+        return 'Simples';
+      case Complexity.medium:
+        return 'Normal';
+      case Complexity.hard:
+        return 'Difícil';
+      default:
+        return 'Desconhecida';
+    }
+  }String get costText {
+    switch (cost) {
+      case Cost.cheap:
+        return 'Barato';
+      case Cost.fair:
+        return 'Justo';
+      case Cost.expensive:
+        return 'Caro';
+      default:
+        return 'Desconhecida';
+    }
+  }
 }
